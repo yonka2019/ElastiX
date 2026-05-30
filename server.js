d4ee6400aa6094332518a8690db89376581da503
yonka <yonka2003@gmail.com>
2026-05-30 16:04:18 +0300

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
 import http from 'node:http';
 import fs from 'node:fs';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+  import { fileURLToPath } from 'node:url';
 import { makeElasticHandlers } from './server/elasticApi.js';
 
 const { handleConfig, handleCount } = makeElasticHandlers(process.env);
